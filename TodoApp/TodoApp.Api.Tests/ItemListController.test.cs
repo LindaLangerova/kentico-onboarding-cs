7b4bbde454ea22677b44d73c33e99a059926fb35
Linda Langerova <LindaL@kentico.com>
2018-01-09 15:33:39 +0100

    KA-211 Create dummy controller with unit tests

---
+++ TodoApp/TodoApp.Api.Tests/ItemListController.test.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Mime;
+using System.Web.Http.Results;
+using NUnit.Framework;
+using TodoApp.Api.Controllers;
+using TodoApp.Api.Models;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace TodoApp.Api.Tests
+{
+    [TestFixture]
+    public class ItemListControllerTest
+    {
+        private ItemListController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new ItemListController();
+        }
+
+        [Test]
+        public void GetAllItemsTest()
+        {
+            var allItems = _controller.GetAllItems();
+
+            Assert.AreEqual(allItems, ItemListController.ItemList);
+        }
+
+        [Test]
+        public void GetItemTest()
+        {
+            var item = _controller.GetItem("0");
+
+            Assert.AreEqual("Make a cofee", item.Text);
+        }
+
+        [Test]
+        public void AddNewItemTest()
+        {
+            ItemModel item = new ItemModel {Id = "4", Text = "New data"};
+            var result = _controller.AddNewItem(item);
+
+            Assert.AreSame(result.GetType(), typeof(OkResult));
+        }
+
+        [Test]
+        public void UpdateItemTest()
+        {
+            var result = _controller.UpdateItem("0", new ItemModel {Id = "0", Text ="New data"});
+
+            Assert.AreSame(result.GetType(), typeof(OkResult));
+        }
+
+        [Test]
+        public void DeleteItemTest()
+        {
+            var result = _controller.DeleteItem("2");
+
+            Assert.AreSame(result.GetType(), typeof(OkResult));
+        }
+
+    }
+}
---
+++ TodoApp/TodoApp.Api/Controllers/ItemListController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Microsoft.Web.Http;
+using TodoApp.Api.Models;
+
+namespace TodoApp.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    public class ItemListController : ApiController
+    {
+         private static readonly ItemModel[] DefaultItems =
+        {
+            new ItemModel {Id = "0", Text = "Make a cofee"},
+            new ItemModel {Id = "1", Text = "Make second coffee"},
+            new ItemModel {Id = "2", Text = "Make third cofffee"},
+            new ItemModel {Id = "3", Text = "Coffee is awesome as well as Kentico is"}
+        };
+        public static readonly List<ItemModel> ItemList = new List<ItemModel>(DefaultItems);
+
+        [HttpGet]
+        public IEnumerable<ItemModel> GetAllItems()
+        {
+            return ItemList;
+        }
+
+        [HttpGet]
+        public ItemModel GetItem(string id)
+        {
+            var item = ItemList.FirstOrDefault(t => t.Id == id);
+
+            return item;
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddNewItem(ItemModel item)
+        {
+            ItemList.Add(item);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateItem(string id, ItemModel item)
+        {
+            var selectedItem = ItemList.FirstOrDefault(t => t.Id == id);
+            if (selectedItem != null) selectedItem.Text = item.Text;
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteItem(string id)
+        {
+            var selectedItem = ItemList.FirstOrDefault(t => t.Id == id);
+            ItemList.Remove(selectedItem);
+
+            return Ok();
+        }
+    }
+}
