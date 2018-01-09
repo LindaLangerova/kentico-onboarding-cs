840263c5610aab335af4df7ef109cbbe68c9e11e
Linda Langerova <LindaL@kentico.com>
2018-01-09 13:39:49 +0100

    KA-210 Simplify project

--- TodoApp.Api/TodoApp.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TodoApp.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- TodoApp.Api/TodoApp.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TodoApp.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
