11dae6cfdb9de4141db0b2bff26e0abddac0192c
Linda Langerova <LindaL@kentico.com>
2018-01-16 09:49:48 +0100

    KA-241 Remove unused usings

--- TodoApp/TodoApp.Api/Controllers/ItemListController.cs
+++ TodoApp/TodoApp.Api/Controllers/ItemListController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Results;
 using Microsoft.Web.Http;
 using TodoApp.Api.Models;
 
