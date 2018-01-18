fc795e27db26c0118016ee5caa925b3a0e144697
Linda Langerova <LindaL@kentico.com>
2018-01-18 18:35:58 +0100

    KA-241 Change values returned from controller

--- TodoApp/TodoApp.Api/Controllers/ItemListController.cs
+++ TodoApp/TodoApp.Api/Controllers/ItemListController.cs
@@ -16,7 +16,7 @@ namespace TodoApp.Api.Controllers
 
         public static ItemModel[] ItemList =
         {
-            new ItemModel {Id = Guid.Parse("c5cc89a0-ab8d-4328-9000-3da679ec02d3"), Text = "Make a cofee"},
+            new ItemModel {Id = Guid.Parse("c5cc89a0-ab8d-4328-9000-3da679ec02d3"), Text = "Make a coffee"},
             new ItemModel {Id = Guid.Parse("55b0d56d-48d7-4f93-bd73-e4b801e26faa"), Text = "Make second coffee"},
             new ItemModel {Id = Guid.Parse("c5cc89a0-ab8d-4328-9000-3da679ec02d2"), Text = "Add some coffee"},
             new ItemModel {Id = Guid.Parse("250be0cc-438e-46cc-a0fe-549f4d3409e2"), Text = "Coffee overflow"}
