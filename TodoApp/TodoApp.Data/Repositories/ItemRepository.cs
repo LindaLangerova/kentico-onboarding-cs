bffe573a4b62a3fcc369267d1bde663a78bf0a96
Linda Langerova <LindaL@kentico.com>
2018-01-23 13:25:00 +0100

    KA-242 Move ItemList to repository

--- TodoApp/TodoApp.Data/Repositories/ItemRepository.cs
+++ TodoApp/TodoApp.Data/Repositories/ItemRepository.cs
@@ -7,11 +7,19 @@ using TodoApp.Contract.Models;
 
 namespace TodoApp.Data.Repositories
 {
-    class ItemRepository:IItemRepository
+    public class ItemRepository:IItemRepository
     {
+        public static ItemModel[] ItemList =
+        {
+            new ItemModel {Id = Guid.Parse("c5cc89a0-ab8d-4328-9000-3da679ec02d3"), Text = "Make a coffee"},
+            new ItemModel {Id = Guid.Parse("55b0d56d-48d7-4f93-bd73-e4b801e26faa"), Text = "Make second coffee"},
+            new ItemModel {Id = Guid.Parse("c5cc89a0-ab8d-4328-9000-3da679ec02d2"), Text = "Add some coffee"},
+            new ItemModel {Id = Guid.Parse("250be0cc-438e-46cc-a0fe-549f4d3409e2"), Text = "Coffee overflow"}
+        };
+        
         public IEnumerable<ItemModel> GetAll()
         {
-            throw new NotImplementedException();
+            return ItemList;
         }
 
         public ItemModel Get(Guid id)
