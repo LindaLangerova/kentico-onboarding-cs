26eb5f127a8106c535a232fa18058bf6f97d1941
Linda Langerova <LindaL@kentico.com>
2018-01-23 13:19:34 +0100

    KA-242 Add repository interface and class

---
+++ TodoApp/TodoApp.Data/Repositories/IItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Contract.Models;
+
+namespace TodoApp.Data.Repositories
+{
+    public interface IItemRepository
+    {
+        IEnumerable<ItemModel> GetAll();
+        ItemModel Get(Guid id);
+        ItemModel Update(Guid id, ItemModel item);
+        ItemModel Add(ItemModel item);
+        void Delete(Guid id);
+    }
+}
---
+++ TodoApp/TodoApp.Data/Repositories/ItemRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Contract.Models;
+
+namespace TodoApp.Data.Repositories
+{
+    class ItemRepository:IItemRepository
+    {
+        public IEnumerable<ItemModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ItemModel Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ItemModel Update(Guid id, ItemModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ItemModel Add(ItemModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
