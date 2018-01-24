9dd540d843065e07c9e80ff301ce5ca78d2c8c87
Linda Langerova <LindaL@kentico.com>
2018-01-24 20:50:57 +0100

    KA-242 Extend controller methods

--- TodoApp/TodoApp.Api/Controllers/ItemListController.cs
+++ TodoApp/TodoApp.Api/Controllers/ItemListController.cs
@@ -26,18 +26,33 @@ namespace TodoApp.Api.Controllers
             => await Task.FromResult(Ok(_repository.GetAll()));
         
         [Route(Id)]
-        public async Task<IHttpActionResult> GetAsync(Guid id) 
-            => await Task.FromResult(Ok(_repository.Get(id)));
-        
+        public async Task<IHttpActionResult> GetAsync(Guid id)
+        {
+            var item = _repository.Get(id);
+            if (item == null)
+                return NotFound();
+
+            return await Task.FromResult(Ok(item));
+        }
+
         public async Task<IHttpActionResult> PostAsync(ItemModel item)
         {
+            if (item?.Text == null)
+                return BadRequest();
+
             var newItem = _repository.Add(item);
+
             return await Task.FromResult(Created($"api/itemlist/{newItem.Id}", newItem));
         }
         
         [Route(Id)]
-        public async Task<IHttpActionResult> PutAsync(Guid id, ItemModel item) 
-            => await Task.FromResult(Ok(_repository.Update(id,item)));
+        public async Task<IHttpActionResult> PutAsync(Guid id, ItemModel item)
+        {
+            if (item?.Text == null || item.Id != id)
+                return BadRequest();
+            var updatedItem = _repository.Update(id, item);
+            return await Task.FromResult(Ok(updatedItem));
+        }
 
         [Route(Id)]
         public async Task<IHttpActionResult> DeleteAsync(Guid id)
