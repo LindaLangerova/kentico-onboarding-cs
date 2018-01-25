8af54fd12c29153038725ff557d9258ada1ec022
Linda Langerova <LindaL@kentico.com>
2018-01-25 10:52:44 +0100

    KA-242 Symplify ItemRepository.cs and move its interface to Contract

--- TodoApp/src/TodoApp.Contract/Repositories/IItemRepository.cs
+++ TodoApp/src/TodoApp.Contract/Repositories/IItemRepository.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using TodoApp.Contract.Models;
 
-namespace TodoApp.Data.Repositories
+namespace TodoApp.Contract.Repositories
 {
     public interface IItemRepository
     {
