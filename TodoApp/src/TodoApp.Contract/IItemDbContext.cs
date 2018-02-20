0595fc9a102d5720a7bfd26eac454ad8e2021974
Linda Langerova <LindaL@kentico.com>
2018-02-20 12:54:44 +0100

    KA-243 Add DbContext

---
+++ TodoApp/src/TodoApp.Contract/IItemDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.Entity;
+using TodoApp.Contract.Models;
+
+namespace TodoApp.Contract
+{
+    public interface IItemDbContext: IDisposable
+    {
+        IDbSet<ItemModel> Items { get; set; }
+    }
+}
