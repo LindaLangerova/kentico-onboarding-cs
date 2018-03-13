17ec3b6bae3fbecc6538a5492f0e33715204bc11
Linda Langerova <LindaL@kentico.com>
2018-03-13 06:41:19 +0100

    KA-243 Upgrade repository

--- TodoApp/src/TodoApp.Data/Repositories/ItemRepository.cs
+++ TodoApp/src/TodoApp.Data/Repositories/ItemRepository.cs
@@ -1,28 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using TodoApp.Contract.Models;
 using TodoApp.Contract.Repositories;
+using TodoApp.Data.Contexts;
 
 namespace TodoApp.Data.Repositories
 {
     public class ItemRepository:IItemRepository
     {
-        internal static ItemModel[] ItemList =
+        private MongoDbContext _db;
+
+        public ItemRepository(MongoDbContext db)
         {
-            new ItemModel {Id = Guid.Parse("c5cc89a0-ab8d-4328-9000-3da679ec02d3"), Text = "Make a coffee"},
-            new ItemModel {Id = Guid.Parse("55b0d56d-48d7-4f93-bd73-e4b801e26faa"), Text = "Make second coffee"},
-            new ItemModel {Id = Guid.Parse("c5cc89a0-ab8d-4328-9000-3da679ec02d2"), Text = "Add some coffee"},
-            new ItemModel {Id = Guid.Parse("250be0cc-438e-46cc-a0fe-549f4d3409e2"), Text = "Coffee overflow"}
-        };
+            _db = db;
+        }
 
-        public IEnumerable<ItemModel> GetAll() => ItemList;
+        public List<Item> GetAll() => _db._itemsCollection.Find(new BsonDocument()).ToListAsync().Result;
 
-        public ItemModel Get(Guid id) => ItemList[0];
+        public Item Get(Guid id) => _db._itemsCollection.Find(item => item.Id == id).First();
 
-        public ItemModel Add(ItemModel item) => ItemList[0];
+        public string Add(Item item)
+        {
+             _db._itemsCollection.InsertOneAsync(item).Wait();
+            return item.Text;
+        }
+        
+        public Item Update(Guid id, Item item) => throw new NotImplementedException();
 
-        public ItemModel Update(Guid id, ItemModel item) => ItemList[0];
-
-        public void Delete(Guid id){}
+        public void Delete(Guid id) => throw new NotImplementedException();
     }
 }
